b8350fc2cd26906fb91d7cfd1d994b864e83aa2b
THIERRY-DEV-ENG <thierrynishimwe936@gmail.com>
2026-03-12 12:01:34 +0200

    simple data types executiton in TS

---
+++ TypeScript.TS.js
@@ -0,0 +1,21 @@
+var age = 21;
+console.log(age);
+var Name = "thierry";
+console.log(Name);
+var scores = [10, 20, 30, 40, 50];
+console.log(scores);
+var User = {
+    name: "thierry",
+    age: 21
+};
+console.log(User);
+var User2 = { id: "123", name: "thierry", age: 21 };
+var Status = "active";
+console.log(User2);
+console.log(Status);
+var Person = { name: "thierry", age: 21 };
+console.log(Person);
+var Employee = { name: "thierry", age: 21, jobTitle: "developer" };
+console.log(Employee);
+var AdminUser = { isAdmin: true, name: "thierry" };
+console.log(AdminUser);
