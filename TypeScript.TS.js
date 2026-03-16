f0dc930e46428744152be2e03daa082fd45600c4
THIERRY-DEV-ENG <thierrynishimwe936@gmail.com>
2026-03-16 23:55:49 +0200

    type async

--- TypeScript.TS.js
+++ TypeScript.TS.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = navBar;
 var age = 21;
 console.log(age);
 var Name = "thierry";
@@ -13,12 +16,39 @@ var User2 = { id: "123", name: "thierry", age: 21 };
 var Status = "active";
 console.log(User2);
 console.log(Status);
+var myName = "thierry";
+console.log(myName);
+var string = "1" | "2" | "3";
+var Name = "1";
+console.log(Name);
 var Person = { name: "thierry", age: 21 };
 console.log(Person);
 var Employee = { name: "thierry", age: 21, jobTitle: "developer" };
 console.log(Employee);
 var AdminUser = { isAdmin: true, name: "thierry" };
 console.log(AdminUser);
-string = "1" | "2" | "3";
-var Name = "1";
-console.log(Name);
+function navBar() {
+    var _a = useState(0), open = _a[0], setOpen = _a[1];
+}
+function addNumbers(a, b) {
+    return a + b;
+}
+var result = addNumbers(5, 3);
+console.log(result);
+var userId;
+userId = 101;
+console.log(userId);
+userId = "A45";
+console.log(userId);
+var age = 20;
+if (age >= 18) {
+    console.log("Adult");
+}
+else {
+    console.log("Minor");
+}
+function greet(name) {
+    console.log("Hello " + name);
+}
+greet("Sam");
+greet();
